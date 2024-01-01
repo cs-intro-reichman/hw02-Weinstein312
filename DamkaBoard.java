8f65526ab259c106729471afc6a35b0added7b62
Weinstein312 <jacob.weinstein@runi.ac.il>
2024-01-01 19:34:56 +0200

    Perfect & Damka

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -4,5 +4,17 @@
 public class DamkaBoard {
 	public static void main(String[] args) {
 		//// Put your code here
+		int num = Integer.parseInt(args[0]);
+
+		for (int i = 0; i < num; i++) {
+			for (int j = 0; j < num; j++) {
+				System.out.print("* ");
+			}
+			System.out.println("");
+
+			if (i % 2 == 0) {
+				System.out.print(" ");
+			}
+		}
 	}
 }
--- Perfect.java
+++ Perfect.java
@@ -4,5 +4,23 @@
 public class Perfect {
 	public static void main (String[] args) {
 		//// Put your code here
+		int num = Integer.parseInt(args[0]);
+		int counter = 1;
+		String str = num + " is a perfect number since " + num + " = 1";
+
+		for (int i = 2; i < num; i++) {
+			if (num % i == 0) {
+				//System.out.println(i);
+				str = str + " + " + i;
+				counter += i;
+			}
+		}
+
+		if (counter == num) {
+			System.out.println(str);
+		}
+		else {
+			System.out.println(num + " is not a perfect number");
+		}
 	}
 }
